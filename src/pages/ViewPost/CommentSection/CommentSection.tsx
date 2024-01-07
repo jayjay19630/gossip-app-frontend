d8890326c51e0e72f747b77da24fc38b5fb47c2b
positivelyjon <positivelyjon@gmail.com>
2024-01-07 18:49:29 +0800

    Add message when there are zero comments

--- src/pages/ViewPost/CommentSection/CommentSection.tsx
+++ src/pages/ViewPost/CommentSection/CommentSection.tsx
@@ -19,10 +19,13 @@ export const CommentSection = (props: CommentsType) => {
 
     const comments = props.comments
     const postid = props.postid;
+    const numberOfComments = comments.length
 
     return (
         <div className='comment-section'>
             <CreateComment postid={postid}/>
+            <div className="comment-number">{numberOfComments} comments</div>
+            <div className='zero-comments'>{numberOfComments === 0 && "This comment section is empty. Add something to the conversation!"}</div>
             {comments.map(comment => <Comment key={comment.id} content={comment.content} username={comment.username} created_at={comment.created_at}></Comment>)}
         </div>
     )
