22915d9c3a2ce6bab83cb1982cf2db9bbfeaa0e4
positivelyjon <positivelyjon@gmail.com>
2024-01-14 20:52:46 +0800

    Fix bug where updating posts would not work

--- src/utils/updatePost.tsx
+++ src/utils/updatePost.tsx
@@ -3,8 +3,8 @@ import { URL_NAME } from "../data/url";
 export const updatePost = (title: string, content: string, tagsArr: number[], postId: string) => {
 
     //fetch hook for updating post by id
-    return fetch(`${URL_NAME}/posts/${postId}/update`, {
-            method: 'POST',
+    return fetch(`${URL_NAME}/posts/${postId}`, {
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token') 
